bf8b1e8818d003f009fdb3205181feb42dce7e93
Rampathoti Ajithkumar <rampathotiajithkumar9330@gmail.com>
2025-06-24 22:02:37 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import AuthContextProvider from './Context/AuthContext'
-import ReactDOM from 'react-dom/client';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
 ReactDOM.render(
   <React.StrictMode>
